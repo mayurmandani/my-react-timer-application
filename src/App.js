daf9b995720a1b689ffbfa4ee12cc1259d4d2bca
Mayur Mandani <mmandani9@gmail.com>
2024-06-14 20:06:07 +0530

    Removed the unused files and images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[timer, setTimer] = useState(0);
+  const[timerInterval, setTimerInterval] = useState(null);
+
+  // Function to start the timer
+  const startTimer = () => {
+      setTimerInterval(
+        // use setInterval to update the timer every 1000 miliseconds(1 second)
+        setInterval(() => {
+              setTimer((prev) => prev + 1);
+        }, 1000));
+  };
+
+  // Function to pause the timer
+  const pauseTimer = () => {
+    // Clear the interval to stop the timer from updating
+     clearInterval(timerInterval);
+  };
+
+  // Function to reset the timer
+  const resetTimer = () => {
+    // Reset the timer value to 0
+    setTimer(0);
+    // Clear the Interval to stop the timer
+    clearInterval(timerInterval);
+  };
+
+  // Render the timer and buttons in  the component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Timer : {timer}</p>
+      <div>
+        <button onClick={startTimer}>Start</button> &nbsp;
+        <button onClick={pauseTimer}>Pause</button> &nbsp;
+        <button onClick={resetTimer}>Reset</button> &nbsp;
+      </div>
     </div>
   );
 }
